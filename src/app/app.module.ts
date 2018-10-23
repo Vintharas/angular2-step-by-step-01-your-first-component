514da1f20e962346ece413067d588e13792f8b73
Jaime González García <jaime.gg83@gmail.com>
2018-10-23 17:03:31 +0200

    Added people list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PeopleListComponent } from './people-list/people-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/person.ts
@@ -0,0 +1,5 @@
+export interface Person {
+  name: string;
+  weight: number;
+  height: number;
+}
